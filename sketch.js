ec8ed0171ed2c908e4b7ddd5673967be91e67d6c
mkhavin-avenues <michael_khavin@avenues-ny.org>
2024-12-10 11:15:14 -0500

    Update sketch.js
    Add Mouse Interaction

--- sketch.js
+++ sketch.js
@@ -16,24 +16,33 @@ function draw() {
   }
 }
 
+function mousePressed() {
+  for (let i = bubbles.length - 1; i >= 0; i--) { 
+    let d = dist(mouseX, mouseY, bubbles[i].x, bubbles[i].y);
+    if (d < bubbles[i].size / 2) {
+      bubbles.splice(i, 1); 
+    }
+  }
+}
+
 class Bubble {
   constructor(x, y, size) {
     this.x = x;
     this.y = y;
     this.size = size;
-    this.color = color(random(255), random(255), random(255)); 
-    this.xSpeed = random(-2, 2); 
-    this.ySpeed = random(-2, 2); 
+    this.color = color(random(255), random(255), random(255));
+    this.xSpeed = random(-2, 2);
+    this.ySpeed = random(-2, 2);
   }
 
   move() {
-    this.x += this.xSpeed; 
-    this.y += this.ySpeed; 
+    this.x += this.xSpeed;
+    this.y += this.ySpeed;
   }
 
   checkEdges() {
     if (this.x < 0 || this.x > width) {
-      this.xSpeed *= -1;
+      this.xSpeed *= -1; 
       this.changeColor();
     }
     if (this.y < 0 || this.y > height) {
@@ -43,7 +52,7 @@ class Bubble {
   }
 
   changeColor() {
-    this.color = color(random(255), random(255), random(255));
+    this.color = color(random(255), random(255), random(255)); 
   }
 
   display() {
