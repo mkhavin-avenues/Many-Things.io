431b9159ef587a4c73f90a4537b609ccb9557402
mkhavin-avenues <michael_khavin@avenues-ny.org>
2024-12-10 11:13:42 -0500

    Update sketch.js
    Add Edge Detection and Color Change

--- sketch.js
+++ sketch.js
@@ -11,6 +11,7 @@ function draw() {
   background(0);
   for (let bubble of bubbles) {
     bubble.move();
+    bubble.checkEdges();
     bubble.display();
   }
 }
@@ -20,19 +21,34 @@ class Bubble {
     this.x = x;
     this.y = y;
     this.size = size;
-    this.color = color(255); // White
-    this.xSpeed = random(-2, 2); // Random horizontal speed
-    this.ySpeed = random(-2, 2); // Random vertical speed
+    this.color = color(random(255), random(255), random(255)); 
+    this.xSpeed = random(-2, 2); 
+    this.ySpeed = random(-2, 2); 
   }
 
   move() {
-    this.x += this.xSpeed; // Update x position
-    this.y += this.ySpeed; // Update y position
+    this.x += this.xSpeed; 
+    this.y += this.ySpeed; 
+  }
+
+  checkEdges() {
+    if (this.x < 0 || this.x > width) {
+      this.xSpeed *= -1;
+      this.changeColor();
+    }
+    if (this.y < 0 || this.y > height) {
+      this.ySpeed *= -1; 
+      this.changeColor();
+    }
+  }
+
+  changeColor() {
+    this.color = color(random(255), random(255), random(255));
   }
 
   display() {
     fill(this.color);
     noStroke();
-    ellipse(this.x, this.y, this.size); // Draw the bubble
+    ellipse(this.x, this.y, this.size); 
   }
 }
